9bd014f9a1724b6d79131302163fa18a14dad639
pkhead <chromosoze@gmail.com>
2023-11-16 20:02:14 -0600

    export animations, compress cells

--- src/ase.lua
+++ src/ase.lua
@@ -1,6 +1,7 @@
 --[[
 Module to handle import/export of Aseprite files
 --]]
+local ffi = require("ffi")
 
 local Aseprite = {}
 
@@ -9,6 +10,106 @@ local Aseprite = {}
 ---@return string
 local function strpack(format, ...)
     return love.data.pack("string", format, ...) --[[@as string]]
+end
+
+---@param workspace Workspace
+local function parseWorkspace(workspace)
+    local celImages = {} ---@type {[integer]: love.ImageData[]}
+
+    --local frames = {} ---@type {[ImageQuad]: }
+    local maxWidth, maxHeight = 0, 0
+
+    -- get the size of a rectangle that can fit all quads
+    -- accounting for center position
+    local numQuads = 0
+    for _, quad in pairs(workspace.quads) do
+        local newWidth = 2 * math.abs(quad.cx - quad.w/2) + quad.w
+        local newHeight = 2 * math.abs(quad.cy - quad.h/2) + quad.h
+        
+        maxWidth = math.max(maxWidth, newWidth)
+        maxHeight = math.max(maxHeight, newHeight)
+
+        numQuads=numQuads+1
+    end
+
+    -- create cel chunks
+    local quadIndex = 1
+    for id, quad in pairs(workspace.quads) do
+        local imageData = love.image.newImageData(maxWidth, maxHeight)
+        imageData:paste(quad.image,
+            math.floor(maxWidth/2 - quad.cx),
+            math.floor(maxHeight/2 - quad.cy),
+            0, 0, quad.w, quad.h
+        )
+        
+        celImages[id] = imageData
+    end
+
+    -- tags
+    local timeline = {} ---@type {id: string, duration: integer}[]
+    local tags = {} ---@type {from: integer, to: integer, name: string}[]
+    local usedFrames = {} ---@type {[integer]: boolean}
+
+    local frameIndex = 0
+    for _, anim in ipairs(workspace.animations) do
+        local from = frameIndex
+
+        for _, frame in ipairs(anim.frames) do
+            usedFrames[frame] = true
+
+            table.insert(timeline, {
+                id = frame,
+                duration = math.floor(anim.frameLen * 16)
+            })
+            frameIndex=frameIndex+1
+        end
+
+        local to = frameIndex-1
+
+        if anim.doLoop then
+            table.insert(tags, {
+                from = from,
+                to = to + anim.loopPoint - 1,
+                name = anim.name .. "-loop"
+            })
+        end
+           
+        if not anim.doLoop or anim.loopPoint ~= 1 then
+            table.insert(tags, {
+                from = from,
+                to = to,
+                name = anim.name
+            })
+        end
+    end
+
+    -- insert unused frames
+    for id, _ in pairs(workspace.quads) do
+        if not usedFrames[id] then
+            table.insert(timeline, {
+                id = id,
+                duration = 100
+            })
+        end
+    end
+
+    return {
+        width = maxWidth,
+        height = maxHeight,
+        celImages = celImages,
+        timeline = timeline,
+        tags = tags
+    }
+end
+
+---@param frameOut Queue
+---@param chunkQueue Queue
+---@param chunkType integer
+local function commitChunk(frameOut, chunkQueue, chunkType)
+    local chunkData = chunkQueue:concat()
+    frameOut:push(strpack("<I4", string.len(chunkData) + 6))
+    frameOut:push(strpack("<I2", chunkType))
+    frameOut:push(chunkData)
 end
 
 ---@class Queue
@@ -30,29 +131,13 @@ end
 
 ---@param workspace Workspace
 function Aseprite.export(workspace)
-    local frames = {} ---@type love.ImageData[]
-
-    local maxWidth, maxHeight = 0, 0
-
-    -- get the size of a rectangle that can fit all quads
-    -- accounting for center position
-    local numQuads = 0
-    for _, quad in pairs(workspace.quads) do
-        local newWidth = 2 * math.abs(quad.cx - quad.w/2) + quad.w
-        local newHeight = 2 * math.abs(quad.cy - quad.h/2) + quad.h
-        
-        maxWidth = math.max(maxWidth, newWidth)
-        maxHeight = math.max(maxHeight, newHeight)
-
-        numQuads=numQuads+1
-    end
-
+    local aseData = parseWorkspace(workspace)
     local aseOut = Queue.new()
 
     -- file header (without file size)
     aseOut:push(strpack("<I2", 0xA5E0)) -- magic number
-    aseOut:push(strpack("<I2", numQuads)) -- frame count
-    aseOut:push(strpack("<I2I2", maxWidth, maxHeight)) -- frame size
+    aseOut:push(strpack("<I2", #aseData.timeline)) -- frame count
+    aseOut:push(strpack("<I2I2", aseData.width, aseData.height)) -- frame size
     aseOut:push(strpack("<I2", 32)) -- RGBA
     aseOut:push(strpack("<I4", 0)) -- no layer opacity
     aseOut:push(strpack("<I2", 0)) -- deprecated
@@ -61,88 +146,98 @@ function Aseprite.export(workspace)
     aseOut:push(strpack("<I2", 0)) -- number of colors
     aseOut:push(string.rep("\0", 94)) -- reserved
 
-    -- frame data
-    local quadIndex = 1
-    for _, quad in pairs(workspace.quads) do
+    -- table to map quad ID to the first appearance of it
+    -- in the timeline. this is to link them together.
+    local celSources = {} ---@type {[integer]: integer}
+    
+    -- write timeline
+    for i, frame in ipairs(aseData.timeline) do
         local frameOut = Queue.new()
 
         frameOut:push(strpack("<I2", 0xF1FA)) -- magic number
 
-        -- spec requires that first frame stores a Layer Chunk
-        -- which means first chunk has 2 chunks, not 1
-        if quadIndex == 1 then
-            frameOut:push(strpack("<I2", 2))
+        -- first frame stores a Layer and Frame Tags Chunk
+        -- which means first chunk has 3 chunks instead of 1
+        if i == 1 then
+            frameOut:push(strpack("<I2", 3))
         else
             frameOut:push(strpack("<I2", 1))
         end
 
-        frameOut:push(strpack("<I2", 1000)) -- frame duration in ms
+        frameOut:push(strpack("<I2", frame.duration)) -- frame duration in ms
         frameOut:push(string.rep("\0", 6)) -- reserved
 
-        -- layer chunk in first frame
-        if quadIndex == 1 then
-            local chunkQueue = Queue.new()
+        if i == 1 then
+            do -- layer chunk
+                local chunkQueue = Queue.new()
+    
+                chunkQueue:push(strpack("<I2I2I2I2I2I2I1c3s2",
+                    3,          -- WORD     flags: visible and editable
+                    0,          -- WORD     layer type
+                    0,          -- WORD     layer child level,
+                    0,          -- WORD     unused
+                    0,          -- WORD  
+                    0,          -- WORD     blend mode (normal)
+                    0,          -- BYTE     opacity (marked unused)
+                    "\0\0\0",   -- BYTE[3]  reserved
+                    "Layer 1"     -- STRING   layer name
+                ))
+    
+                -- commit chunk
+                commitChunk(frameOut, chunkQueue, 0x2004)
+            end
 
-            chunkQueue:push(strpack("<I2I2I2I2I2I2I1c3s2",
-                3,          -- WORD     flags: visible and editable
-                0,          -- WORD     layer type
-                0,          -- WORD     layer child level,
-                0,          -- WORD     unused
-                0,          -- WORD  
-                0,          -- WORD     blend mode (normal)
-                0,          -- BYTE     opacity (marked unused)
-                "\0\0\0",   -- BYTE[3]  reserved
-                "Layer 1"     -- STRING   layer name
-            ))
+            do -- tags chunk
+                local chunkQueue = Queue.new()
 
-            -- commit chunk
-            local chunkData = chunkQueue:concat()
-            frameOut:push(strpack("<I4", string.len(chunkData) + 6))
-            frameOut:push(strpack("<I2", 0x2004))
-            frameOut:push(chunkData)
+                chunkQueue:push(strpack("<I2", #aseData.tags)) -- tag count
+                chunkQueue:push(strpack("<I8", 0)) -- reserved
+                
+                for _, tag in ipairs(aseData.tags) do
+                    chunkQueue:push(strpack("<I2I2I1", tag.from, tag.to, 0)) -- from, to, loop direction (forward)
+                    chunkQueue:push(strpack("<I8", 0)) -- reserved
+                    chunkQueue:push(strpack("<I1I1I1", 0, 0, 0)) -- tag color
+                    chunkQueue:push(strpack("x")) -- zero byte
+                    chunkQueue:push(strpack("s2", tag.name)) -- tag name
+                end
+
+                commitChunk(frameOut, chunkQueue, 0x2018)
+            end
         end
 
         -- cel chunk
         do
             local chunkQueue = Queue.new()
-            chunkQueue:push(strpack("<I2I2I2I1I2c7 I2I2",
+
+            local link = celSources[frame.id] ---@type integer?
+            local celType = 2 -- compressed raw cell
+            if link ~= nil then
+                celType = 1 -- linked cell
+            end
+
+            chunkQueue:push(strpack("<I2I2I2I1I2c7",
                 0,                  -- WORD: layer index
                 0,                  -- WORD: x position
                 0,                  -- WORD: y position
                 255,                -- BYTE: opacity
-                0,                  -- WORD: cel type (raw cell)
-                string.rep("\0", 7),-- reserved
+                celType,            -- WORD: cel type (raw cell)
+                string.rep("\0", 7) -- reserved
     
-                maxWidth,           -- WORD: width
-                maxHeight           -- WORD: height
             ))
-
-            -- write pixel data into chunk queue
-            local ox = math.floor(maxWidth/2 - quad.cx)
-            local oy = math.floor(maxHeight/2 - quad.cy)
-            local w = quad.image:getWidth()
-            local h = quad.image:getHeight()
             
-            for y=0, maxHeight-1 do
-                for x=0, maxWidth-1 do
-                    local mx, my = x - ox, y - oy
-                    local r, g, b, a = 0, 0, 0, 0
+            if link ~= nil then
+                chunkQueue:push(strpack("<I2", link))
+            else
+                celSources[frame.id] = i-1
 
-                    if mx >= 0 and my >= 0 and mx < w and my < h then
-                        r, g, b, a = quad.image:getPixel(mx, my)
-                    end
-
-                    chunkQueue:push(strpack("<I1I1I1I1", r * 255, g * 255, b * 255, a * 255))
-                end
+                -- write raw data
+                local image = aseData.celImages[frame.id] ---@type love.ImageData
+                chunkQueue:push(strpack("<I2I2", image:getWidth(), image:getHeight()))
+                chunkQueue:push(love.data.compress("string", "zlib", image))
             end
 
             -- commit chunk
-            local chunkData = chunkQueue:concat()
-            frameOut:push(strpack("<I4", string.len(chunkData) + 6))
-            frameOut:push(strpack("<I2", 0x2005))
-            frameOut:push(chunkData)
-
-            print("cel chunk")
+            commitChunk(frameOut, chunkQueue, 0x2005)
         end
 
         local frameData = frameOut:concat()
@@ -150,15 +245,13 @@ function Aseprite.export(workspace)
         aseOut:push(frameData)
 
         print("write frame")
-
-        quadIndex = quadIndex + 1
     end
 
     -- commit ase file
     local out = Queue.new()
-    local aseData = aseOut:concat()
-    out:push(strpack("<I4", string.len(aseData)))
-    out:push(aseData)
+    local data = aseOut:concat()
+    out:push(strpack("<I4", string.len(data)))
+    out:push(data)
 
     return out:concat()
 
